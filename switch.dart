7c43008eb27f9d37f60e0786634f4d2bdfa5ae66
Saged00 <segasaged32@gmail.com>
2025-03-14 02:17:47 +0200

    Add files via upload

---
+++ switch.dart
@@ -0,0 +1,22 @@
+void main() {
+  var days = 5;
+  switch (days) {
+    case 1:
+      print("monday");
+
+    case 2:
+      print("thrusday");
+
+    case 3:
+      print("wendesday");
+
+    case 4:
+      print("thusday");
+
+    case 5:
+      print("friday");
+
+    default:
+      print("it is a weekend");
+  }
+}
