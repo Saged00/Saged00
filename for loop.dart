b47d46f4f01717f9fd77224549d00e9b8faa0e84
Saged00 <segasaged32@gmail.com>
2025-04-21 03:58:22 +0200

    Update for loop.dart

--- for loop.dart
+++ for loop.dart
@@ -1,22 +1,5 @@
 void main() {
-  var days = 5;
-  switch (days) {
-    case 1:
-      print("monday");
-
-    case 2:
-      print("thrusday");
-
-    case 3:
-      print("wendesday");
-
-    case 4:
-      print("thusday");
-
-    case 5:
-      print("friday");
-
-    default:
-      print("it is a weekend");
+    for (int x = 1; x < 11; x++) {
+    print(x);
   }
 }
